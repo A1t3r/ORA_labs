f081897880a4d10444d524d9c5ec55cb3f0d3ec9
PoliakovVI <valerider@icloud.com>
2021-04-13 22:57:52 +0300

    finished fishing

--- main.cpp
+++ main.cpp
@@ -1,7 +1,194 @@
 #include <iostream>
 #include <string>
+#include <map>
+#include <vector>
+#include <queue>
+#include <cmath>
 #include <math.h>
 #include <fstream>
+
+
+struct Node {
+	map<char, Node*> links;
+	Node* fake_link = 0;
+	bool isFinish = false;
+	int point = -1;
+};
+
+
+struct Father_Son_Value {
+	Node* father;
+	Node* son;
+	char value;
+};
+
+
+class Bor {
+private:
+	Node* root;
+	vector<bool> found;
+	size_t size = 0;
+
+	void AddPattern(const string& pattern, int point) {
+		Node* current_node = root;
+		Node* new_node = 0;
+
+		for (const char& sym : pattern) {
+			if (current_node->links.find(sym) == current_node->links.end()) {
+				new_node = new Node();
+				current_node->links[sym] = new_node;
+				current_node = new_node;
+			}
+			else {
+				current_node = current_node->links[sym];
+			}
+		}
+		current_node->isFinish = true;
+		current_node->point = point;
+	}
+
+	void DelNode(Node* node) {
+		for (auto& child_node : node->links) {
+			DelNode(child_node.second);
+		}
+		delete node;
+	}
+
+	void Print(Node* node, string prefix) {
+		cout << prefix << node << "|" << node->fake_link << endl;
+		for (auto& child_node : node->links) {
+			Print(child_node.second, prefix + "  ");
+		}
+	}
+
+	void SetFakeLink(Father_Son_Value* fsv) {
+		if (fsv->father == root) {
+			fsv->son->fake_link = root;
+			return;
+		}
+
+		Node* father = fsv->father->fake_link;
+
+		while (father->links.find(fsv->value) == father->links.end()) {
+			if (father == root) {
+				fsv->son->fake_link = root;
+				return;
+			}
+			father = father->fake_link;
+		}
+
+		fsv->son->fake_link = father->links[fsv->value];
+		return;
+	}
+
+public:
+	Bor(const vector<string>& patterns) {
+		size = patterns.size();
+		root = new Node();
+
+		int number = 0;
+		for (const auto& pattern : patterns) {
+			AddPattern(pattern, number);
+			number++;
+		}
+
+		queue<Father_Son_Value*> Fathers_and_Sons;
+
+		root->fake_link = root;
+		for (auto& pair : root->links) {
+			Father_Son_Value* fsv = new Father_Son_Value();
+			fsv->father = root;
+			fsv->son = pair.second;
+			fsv->value = pair.first;
+			Fathers_and_Sons.push(fsv);
+		}
+
+		while (!Fathers_and_Sons.empty()) {
+			Father_Son_Value* current_fsv = Fathers_and_Sons.front();
+			SetFakeLink(current_fsv);
+
+			for (auto& pair : current_fsv->son->links) {
+				Father_Son_Value* fsv = new Father_Son_Value();
+				fsv->father = current_fsv->son;
+				fsv->son = pair.second;
+				fsv->value = pair.first;
+				Fathers_and_Sons.push(fsv);
+			}
+
+			Fathers_and_Sons.pop();
+			delete current_fsv;
+		}
+
+	}
+
+	void FindIn(string& base) {
+		for (int i = 0; i < size; i++) {
+			found.push_back(false);
+		}
+
+		Node* current_node = root;
+
+		for (auto iter = base.begin(); iter < base.end(); iter++) {
+			char item = *iter;
+			while (true) {
+				if (current_node->links.find(item) != current_node->links.end()) {
+					current_node = current_node->links[item];
+					break;
+				}
+				else {
+					if (current_node == root) {
+						break;
+					}
+					current_node = current_node->fake_link;
+				}
+			}
+
+			if (current_node->isFinish) {
+				found[current_node->point] = true;
+			}
+			Node* subnode = current_node;
+			while (subnode->fake_link->isFinish) {
+				found[current_node->fake_link->point] = true;
+				subnode = current_node->fake_link;
+			}
+		}
+
+	}
+
+	vector<bool> getFoundCopy() {
+		return found;
+	}
+
+	void Show() {
+		Print(root, "  ");
+	}
+
+	~Bor() {
+		DelNode(root);
+	}
+};
+
+
+vector<bool> Aho_Corasic_algorithm(string& base, vector<string>& patterns) {
+	Bor bor(patterns);
+	bor.FindIn(base);
+	return bor.getFoundCopy();
+}
+
+
+void run_Aho_Corasic_algorithm_demo() {
+	vector<string> pats = { "acc", "ac", "cat", "gcc", "oca", "a", "tua", "tgg" };
+	string base = "ocatuaccbutnotgc";
+	int number = 0;
+	for (auto item : Aho_Corasic_algorithm(base, pats)) {
+		string buf = "not found";
+		if (item == 1) buf = "found"
+		cout << pats[number] << ": " << buf << endl;
+
+		number++:
+	}
+	return;
+}
 
 static bool check_string_part(std::string& text, std::string& mask, size_t pos){
     for(size_t j = 0; j < mask.size(); ++j){
@@ -69,6 +256,9 @@ int ARK(std::string& text, std::string& mask) {
 }
 
 int main() {
+	run_Aho_Corasic_algorithm_demo();
+	return 0;
+
     std::string text;
     std::string mask;
     /*

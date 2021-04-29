4cd2059357cb72323efa68de2b1a3a25f4a65bff
PoliakovVI <valerider@icloud.com>
2021-04-29 13:03:53 +0300

    added speedup

--- main.cpp
+++ main.cpp
@@ -3,21 +3,20 @@
 #include <string>
 #include <ctime>
 #include <algorithm>
+#include <chrono>
 #include <fstream>
+#include <algorithm>
+#include <thread>
+#include <mutex>
+
+#include "Object.h"
 
 using namespace std;
 
-struct Object {
-    int id;
-	int cost = 0;
-	int weight = 0;
-	double quality = 0.0;
-	bool used = false;
-	short use_condition = -1;
-	Object(int obj_cost, int obj_weight, int obj_id) :
-            cost(obj_cost), weight(obj_weight), id(obj_id){
-	};
-};
+mutex m;
+condition_variable cv;
+bool NewComp = false;
+bool FinishThread = false;
 
 int ComputeCost(vector<int> ids, vector<Object>& objects, int W) {
 	int current_W = 0;
@@ -136,6 +135,160 @@ int MDP2(vector<Object>& objects, int W) {
 	return prev[length - 1];
 }
 
+void compute_curr_thread(int& start_index, int& finish_index, int& string_index,
+	vector<int>& prev, vector<int>& curr, const vector<Object>& objects, int& W) {
+	vector<int>& thread_curr = curr;
+	vector<int>& thread_prev = prev;
+
+	while (true) {
+		std::unique_lock<mutex> ul(m);
+		cv.wait(ul, [=]() { return NewComp; });
+
+		if (FinishThread) break;
+
+
+		//cout << "thread " << start_index << ":" << finish_index << endl;
+		for (int item_index = start_index; item_index < finish_index; ++item_index) {
+			int obj_W = objects[string_index - 1].weight;
+			if (obj_W <= W) {
+				int prev_bp = 0;
+				if (item_index >= obj_W) {
+					prev_bp = objects[string_index - 1].cost + thread_prev[item_index - obj_W];
+				}
+
+				thread_curr[item_index] = max(
+					thread_prev[item_index],
+					prev_bp
+				);
+			}
+			else {
+				break;
+			}
+		}
+
+		NewComp = false;
+		cv.notify_one();
+	}
+}
+
+void compute_curr(int& start_index, int& finish_index, int& string_index,
+	vector<int>& prev, vector<int>& curr, const vector<Object>& objects, int& W) {
+
+	for (int item_index = start_index; item_index < finish_index; ++item_index) {
+		int obj_W = objects[string_index - 1].weight;
+		if (obj_W <= W) {
+			int prev_bp = 0;
+			if (item_index >= obj_W) {
+				prev_bp = objects[string_index - 1].cost + prev[item_index - obj_W];
+			}
+
+			curr[item_index] = max(
+				prev[item_index],
+				prev_bp
+			);
+		}
+		else {
+			break;
+		}
+	}
+}
+
+int MDP2_speedup_nt(vector<Object>& objects, int W) {
+	sort(objects.begin(), objects.end(), [](const Object& lhs, const Object& rhs) -> bool
+		{
+			return lhs.weight > rhs.weight;
+		});
+	int length = W + 1;
+	int size = objects.size();
+	int mid = 0;
+	int start_index = 0;
+
+	vector<int> prev(length);
+	vector<int> curr(length);
+	vector<int> barrier(size + 1);
+	int threshold = 100;
+	barrier[size] = length - 1;
+	// step 1: computing barrier
+	for (int i = size - 1; i >= 0; --i) {
+		barrier[i] = barrier[i + 1] - objects[i].weight;
+	}
+
+	for (int string_index = 1; string_index <= size; ++string_index) {
+		start_index = max(barrier[string_index], 0);
+		if (false) {
+			mid = (start_index + length) / 2;
+			thread left_work(compute_curr, ref(start_index), ref(mid), ref(string_index),
+				ref(prev), ref(curr), ref(objects), ref(W));
+			compute_curr(mid, length, string_index, prev, curr, objects, W);
+			left_work.join();
+		}
+		else {
+			//cout << start_index << ":" << length << endl;
+			compute_curr(start_index, length, string_index, prev, curr, objects, W);
+		}
+		swap(prev, curr);
+	}
+	return prev[length - 1];
+}
+
+int MDP2_speedup(vector<Object>& objects, int W) {
+	sort(objects.begin(), objects.end(), [](const Object& lhs, const Object& rhs) -> bool
+		{
+			return lhs.weight > rhs.weight;
+		});
+	int length = W + 1;
+	int size = objects.size();
+	int mid = 0;
+	int start_index = 0;
+	int obj_id = 0;
+
+	vector<int> prev(length);
+	vector<int> curr(length);
+	vector<int> barrier(size + 1);
+	int threshold = 100;
+	barrier[size] = length - 1;
+	// step 1: computing barrier
+	for (int i = size - 1; i >= 0; --i){
+		barrier[i] = barrier[i + 1] - objects[i].weight;
+	}
+
+	thread left_work(compute_curr_thread, ref(start_index), ref(mid), ref(obj_id),
+		ref(prev), ref(curr), ref(objects), ref(W));
+
+	for (int string_index = 1; string_index <= size; ++string_index) {
+		//cout << string_index << endl;
+		obj_id = string_index;
+		start_index = max(barrier[string_index], 0);
+		if (length - start_index > threshold) {
+			mid = (start_index + length) / 2;
+			{
+				std::unique_lock<std::mutex> ul(m);
+				NewComp = true;
+				cv.notify_one();
+			}
+			//cout << "main " << mid << ":" << length << endl;
+			compute_curr(mid, length, string_index, prev, curr, objects, W);
+
+			std::unique_lock<std::mutex> ul(m);
+
+			cv.wait(ul, [=]() { return NewComp == false; });
+
+			ul.unlock();
+		}
+		else {
+			//cout << start_index << ":" << length << endl;
+			compute_curr(start_index, length, string_index, prev, curr, objects, W);
+		}
+		swap(prev, curr);
+	}
+	FinishThread = true;
+	NewComp = true;
+	cv.notify_all();
+	left_work.join();
+
+	return prev[length - 1];
+}
+
 int greedyAlgorithm(vector<Object>& objects, int capacity){
     int res = 0;
     for(auto& obj : objects)
@@ -238,7 +391,34 @@ void branchAndBound(vector<Object> objects, int capacity){
     }
 }
 
+void MDP2_comp() {
+	int W = 18000;
+
+	auto begin = std::chrono::steady_clock::now();
+	int c = MDP2(example, W);
+	auto end = std::chrono::steady_clock::now();
+
+	auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+	std::cout << "Base version: " << c << "$. The time: " << elapsed_ms.count() << " ms\n";
+
+	auto beginsnt = std::chrono::steady_clock::now();
+	int csnt = MDP2_speedup_nt(example, W);
+	auto endsnt = std::chrono::steady_clock::now();
+
+	auto elapsedsnt_ms = std::chrono::duration_cast<std::chrono::milliseconds>(endsnt - beginsnt);
+	std::cout << "Sort-barrier speedup: " << csnt << "$. The time: " << elapsedsnt_ms.count() << " ms\n";
+
+	auto begins = std::chrono::steady_clock::now();
+	int cs = MDP2_speedup(example, W);
+	auto ends = std::chrono::steady_clock::now();
+
+	auto elapseds_ms = std::chrono::duration_cast<std::chrono::milliseconds>(ends - begins);
+	std::cout << "Sort-barrier + 2-proc speedup (only for really large cases): " << cs << "$. The time: " << elapseds_ms.count() << " ms\n";
+}
+
 int main() {
+	MDP2_comp();
+	return 0;
 
     vector<Object> objj = {
             {6, 1, 0},
